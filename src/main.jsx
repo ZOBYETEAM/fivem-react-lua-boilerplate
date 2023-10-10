0db1cbe24e87e9e5fa17f92663bd195070be50b8
TAKZOBYE <takzobye@gmail.com>
2023-10-11 05:29:45 +0700

    Make router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+// Pages
+import Home from './pages/Home.jsx'
+import Bank from './pages/Bank.jsx'
+import Safe from './pages/Safe.jsx'
+import Fight from './pages/Fight.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/bank",
+		element: <Bank />,
+	},
+	{
+		path: "/safe",
+		element: <Safe />,
+	},
+	{
+		path: "/fight",
+		element: <Fight />,
+	},
+]);
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 )
